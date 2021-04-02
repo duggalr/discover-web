50bc6323f015cfebbfc34cee53021be0ea614376
Rahul Duggal <duggalr42@gmail.com>
2021-04-01 23:25:28 -0400

    back.

--- extension/background.js
+++ extension/background.js
@@ -1,71 +1,48 @@
 
-
-// TODO: need live listeners to constantly update database, etc.
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('service worker works');
-
-  //  TODO: we just want all the bookmarks from all folders; (don't need folder organization, etc)
-    // get the date added as well
-  chrome.bookmarks.getTree(
-    function(bookmarkTreeNodes) {
-      // console.log(bookmarkTreeNodes);
-      var children = bookmarkTreeNodes[0]['children']
-      for (i=0; i<=children.length-1; i++) {
-        var li = children[i]['children']
-        getBookMarks(li)
-      }
-    }
-  );
-
-  // Save into database
-  function getBookMarks(arr) {
-    for (i=0; i<=arr.length-1; i++) {
-      var di = arr[i]
-      var bookmark_date = di['dateAdded']  // use unix timestamp in sqlite too
-      var bookmark_title = di['title']
-      var bookmark_url = di['url']
-      var bookmark_id = di['url']
-      
-      // Send off to flask API 
-
-    }
-  }
-
-  query = {
-    'text': '',
-    'startTime': 1609477200, // 2021/01/01 
-    'maxResults': 1000000  
-  }
-  chrome.history.search(query, function(results) {
-    console.log('results length', results.length)
-    for (i=0; i<=results.length-1; i++) {
-      di = results[i]
-      lastVisitedTime = di['lastVisitTime']
-      visitCount = di['visitCount']
-      title = di['title']
-      url = di['url']
-      url_id = di['id']
-    }
-  })
-  query = {
-    'url': 'https://geohot.github.io/blog/'
-  }
-  chrome.history.getVisits(query, function(results){ 
-    console.log(results)
-    for (i=0; i<=results.length-1; i++) {
-      di = results[i]
-      url_id = di['id']
-      visit_id = di['visitId']
-      referrer_visit_id = di['referringVisitId']
-      visitTime = di['visitTime']
-      transition_type = di['transition']
-      
-    }
-    
-  })
+chrome.runtime.onInstalled.addListener( () => {
 
 });
+
+
+chrome.contextMenus.create({
+  "id": "text_snippet",
+  "title": "Save Text",
+  "contexts": ["selection"]
+});
+
+
+chrome.contextMenus.onClicked.addListener(async function(info, tab){
+  var selectedText = info.selectionText
+  var tabUrl = tab.url
+  var tabTitle = tab.title
+  jsonRV = {
+    'tabURL': tabUrl, 'tabTitle': tabTitle, 'selectedText': selectedText
+  }
+  console.log('Selected text: ' + selectedText + ' from tab: '+ tabUrl);
+  // console.log(JSON.stringify(jsonRV));
+  var serverRV = await sendText(JSON.stringify(jsonRV))
+  console.log('server:', serverRV)
+});
+
+
+function sendText(jsonRV) {
+var url = 'http://127.0.0.1:8000/save_text'
+return new Promise((res, rej) => {
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: jsonRV
+      }).then(function(response) {
+      response.json().then(function(val){
+        res(val)
+      })
+    });
+  })
+}
+
 
 
 
---
+++ server_app/server.py
@@ -0,0 +1,139 @@
+# from sanic import Sanic
+# from sanic.response import json
+# import sqlite3
+# from databases import Database
+
+import flask
+from flask import request, render_template, g, session, redirect, url_for, jsonify
+from functools import wraps
+import sqlite3
+from urllib.parse import urlparse
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+app.config["DATABASE"] = '/Users/rahul/Desktop/primary/nlp_projects/project_a/main.db'
+# SECRET_KEY = open("secret_key.txt", 'r').read()
+# app.secret_key = SECRET_KEY
+
+
+def connect_db():
+  sqlite_db = sqlite3.connect(app.config["DATABASE"])
+  sqlite_db.row_factory = sqlite3.Row # to return dicts rather than tuples
+  return sqlite_db
+
+@app.before_request
+def before_request():
+  g.db = connect_db()
+  
+@app.teardown_request
+def teardown_request(exception):
+  db = getattr(g, 'db', None)
+  if db is not None:
+    db.close()
+
+def check_admin(f):  # TODO: wireshark adjusting sessions? need to change authentication approach 
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'admin' not in session:
+      return redirect(url_for('home'))
+
+    if session['admin'] is not True:
+      return redirect(url_for('home'))
+
+    return f(*args, **kwargs)
+  
+  return wrap
+
+
+def format_posts(posts):
+  rv = []
+  for post in posts:
+    title, date = post['title'], post['created']
+    formatted_date = date.split(' ')[0]
+    rv.append({'id': post['id'], 'title': title, 'created': formatted_date, 'body': post['body']})
+  return rv
+
+
+@app.route('/save_text', methods=["POST"])
+def save_text():
+  print('Data', request.json)
+  url = request.json['tabURL']
+  hostname = urlparse(url).netloc  # save because we will use hostname as titles with the saved text underneath;
+  title = request.json['tabTitle']
+  selected_text = request.json['selectedText']
+  sql_str = 'insert into savedText(url, hostname, title, selected_text) values (?, ?, ?, ?)' 
+  g.db.execute(sql_str, (url, hostname, title, selected_text))
+  g.db.commit()
+  return jsonify({'success': 'success'})
+
+
+@app.route('/', methods=["GET"])
+def home():
+  sql_str = 'select * from savedText'
+  cur = g.db.execute(sql_str)
+  rv = cur.fetchall()
+  di = {}
+  for val in rv:
+    di[val['hostname']] = []
+
+  # TODO: should we organize by titles from the same hostname sites? (ie. different saved snippets from same hostname site?)
+  for val in rv:
+    li = di[val['hostname']]
+    li.append({'title': val['title'], 'url': val['url']})
+    di[val['hostname']] = li 
+
+  return render_template('home.html', savedText=di)
+
+
+if __name__ == '__main__':
+  app.run()
+
+
+# export FLASK_APP=app.py
+# export FLASK_ENV=development
+# flask run
+# flask run -h localhost -p 8000
+
+## design of database; #TODO: this will constantly change as we build out more features, etc. 
+
+# CREATE TABLE savedText( id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, hostname TEXT, title TEXT, selected_text TEXT);
+
+# CREATE TABLE url( id INTEGER PRIMARY KEY AUTOINCREMENT, url_id INTEGER, title TEXT, url TEXT);
+# CREATE TABLE visitDetail( id INTEGER PRIMARY KEY AUTOINCREMENT, url_id INTEGER, visit_id INTEGER, referrer_visit_id INTEGER, visitTime REAL, transition_type TEXT, FOREIGN KEY(url_id) REFERENCES url(url_id) );
+# CREATE TABLE bookmark( id INTEGER PRIMARY KEY AUTOINCREMENT, url_id INTEGER, date_added REAL, FOREIGN KEY(url_id) REFERENCES url(url_id));
+# CREATE TABLE history(id INTEGER PRIMARY KEY AUTOINCREMENT, url_id INTEGER, last_visit_time INTEGER, visit_count INTEGER, FOREIGN KEY(url_id) REFERENCES url(url_id));
+
+# {'urlID': '7533', 'url': 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function', 'title': 'async function - JavaScript | MDN', 'visitcount':
+#  2, 'lastVisitedTime': 1617154081598.154, 'visitDetails': [{'id': '7533', 'referringVisitId': '0', 'transition': 'link', 'visitId': '14179', 'visitTime': 1617154080879.8298}, {'id
+# ': '7533', 'referringVisitId': '0', 'transition': 'link', 'visitId': '14180', 'visitTime': 1617154081598.154}, {'id': '7533', 'referringVisitId': '0', 'transition': 'link', 'visit
+# Id': '14252', 'visitTime': 1617191025961.333}, {'id': '7533', 'referringVisitId': '0', 'transition': 'link', 'visitId': '14253', 'visitTime': 1617191026295.164}]}    
+
+# def check_url_duplicate(url_id):
+#   sql_str = 'select exists(select 1 from url where url_id= ? )'
+#   cur = g.db.execute(sql_str, (url_id))
+#   rv = cur.fetchall()
+#   print(rv)
+
+# def save_url(di):
+#   url_id = di['urlID']
+#   check_url_duplicate(url_id)
+#   # url = di['url']
+#   # url_title = di['title']
+#   # sql_str = 'insert into url (url_id, url, url_title) values (?, ?, ?)' 
+#   # g.db.execute(sql_str, (url_id,url,url_title))
+#   # g.db.commit()
+
+# @app.route('/save_history', methods=["POST"])
+# def save_history(request):
+#   print('Hist', request.json)
+#   save_url(request.json)
+  
+#   # url_id = request.json['urlID']
+#   # url = request.json['url']
+#   # url_title = request.json['title']
+#   # visit_count = request.json['visitcount']
+#   # last_visited_time = request.json['lastVisitedTime']
+#   # visit_details = request.json['visitDetails']
+#   return jsonify({'success': 'success'})
+
